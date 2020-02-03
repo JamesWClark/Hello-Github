0d83ebc57c16c4166be1c58be3f2eab227b4ae3e
JW Clark <jwclark@rockhursths.edu>
2020-02-03 08:40:01 -0600

    First python

---
+++ HelloGit.py
@@ -0,0 +1,18 @@
+# comment
+print("Hello, World!")
+a = 6
+b = 5
+print(a + b)
+
+# how to make a list
+myList = [1, 2, 3]
+
+# how to access elements of a list
+print(myList[1])
+
+# how to write a function
+def sumFunction(a, b):
+	return a + b
+
+# how to call a function
+print(sumFunction(2, 20))
